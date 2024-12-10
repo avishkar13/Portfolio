ec64b9fcf51be97c01d04f7c7461907c71489941
Avishkar Deogharia <avishkardeogharia2004@gmail.com>
2024-12-10 19:54:54 +0530

    paths issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,25 +9,24 @@ const About = () => {
   return (
     <>
       <Navbar />
-      <div className="About text-white  lg:w-[97%] xl:w-[92vw] min-h-[92vh] py-16 md:py-16 lg:py-8 mx-auto flex justify-between gap-2  ">
-        <div className="px-2 w-[35%] xl:w-1/3 h-[90vh] lg:flex items-center hidden ">
+      <div className="About text-white lg:w-[97%] xl:w-[92vw] min-h-[92vh] py-16 lg:py-8 mx-auto flex flex-col lg:flex-row justify-between gap-6">
+        <div className="hidden lg:flex w-[35%] xl:w-1/3 h-[90vh] items-center">
           <img
-            className="rounded-[60px] w-[100%] h-[80%]  "
-            src="public\images/Profile pic.jpeg"
-            alt="About Pic"
+            className="rounded-[60px] w-full h-[80%] object-cover"
+            src="/images/Profile pic.jpeg"
+            alt="Avishkar Deogharia"
           />
         </div>
 
-        <div className="Info w-full lg:w-3/4 p-4 ">
+        <div className="Info w-full lg:w-3/4 p-4">
           {/* Toggle section buttons */}
-          <div className="sections w-[65%] md:w-[40%] xl:w-[30%] mx-auto flex justify-around p-4 text-gray-400 rounded-full ">
+          <div className="sections w-[65%] md:w-[40%] xl:w-[30%] mx-auto flex justify-around p-4 text-gray-400 rounded-full">
             <span
               onClick={() => setActiveSection("about")}
               className={`font-mono font-semibold w-full text-center text-xl cursor-pointer ${activeSection === "about" ? "text-white" : "text-gray-400"}`}
             >
               About
             </span>
-            
             <span
               onClick={() => setActiveSection("social")}
               className={`font-mono font-semibold w-full text-center text-xl cursor-pointer ${activeSection === "social" ? "text-white" : "text-gray-400"}`}
@@ -38,27 +37,38 @@ const About = () => {
           <div className="h-[2px] w-[65%] md:w-[40%] xl:w-[30%] mx-auto bg-gray-700 mt-[-10px]"></div>
 
           {/* Display content based on active section */}
-          {activeSection === "about" && (
-            <Aboutinfo />
-          )}
-          
+          {activeSection === "about" && <Aboutinfo />}
           {activeSection === "social" && (
-            <div className="social-info w-[90%] md:w-[50%] h-[50%] mx-auto my-6 p-6 flex flex-col gap-4 ">
-              <Link to="https://www.linkedin.com/in/avishkar-deogharia" target='_blank' className='flex gap-4 items-center'>
-                <img src="public\images/linkedin.svg" alt="LinkedIn" className="w-10 h-10 invert" />
+            <div className="social-info w-[90%] md:w-[50%] mx-auto my-6 p-6 flex flex-col gap-6">
+              <Link
+                to="https://www.linkedin.com/in/avishkar-deogharia"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4 items-center"
+              >
+                <img src="/images/linkedin.svg" alt="LinkedIn Icon" className="w-10 h-10 invert" />
                 <p className="text-gray-300 text-xl">Connect with me on LinkedIn!</p>
               </Link>
-              <Link to="https://github.com/avishkar13" target='_blank' className='flex gap-4 items-center'>
-                <img src="public\images/github.svg" alt="GitHub" className="w-10 h-10 invert" />
+              <Link
+                to="https://github.com/avishkar13"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4 items-center"
+              >
+                <img src="/images/github.svg" alt="GitHub Icon" className="w-10 h-10 invert" />
                 <p className="text-gray-300 text-xl">Check out my GitHub!</p>
               </Link>
-              <Link to="https://www.instagram.com/_avishkar__13/" target='_blank' className='flex gap-4 items-center'>
-                <img src="public\images/instagram.svg" alt="Instagram" className="w-10 h-10 invert" />
+              <Link
+                to="https://www.instagram.com/_avishkar__13/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4 items-center"
+              >
+                <img src="/images/instagram.svg" alt="Instagram Icon" className="w-10 h-10 invert" />
                 <p className="text-gray-300 text-xl">Follow me on Instagram!</p>
               </Link>
             </div>
           )}
-
         </div>
       </div>
     </>
